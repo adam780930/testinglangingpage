caf051b3c98771d6ea525ac3c747eaeeeafb15f0
Adam Tsai <adam78030@gmail.com>
2023-12-02 14:08:58 -0800

    updated linkedin url and icon on front page

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,27 +1,52 @@
-import { Link } from 'react-router-dom';
-
-import classes from './MainNavigation.module.css';
-
+import { Link } from "react-router-dom";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-    return (
-        <header className={classes.header}>
-            <div className={classes.logo}><Link to='/'>AT</Link></div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
-                    <li>
-                        <Link to='/about'>About</Link>
-                    </li>
-                    <li>
-                        <Link to='/contact'>Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>
+        <Link to="/">AT</Link>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/adam780930"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className={classes.github}
+          ></FontAwesomeIcon>
+        </a>
+
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/adam-yilin-tsai"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className={classes.linkedin}
+          ></FontAwesomeIcon>
+        </a>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default MainNavigation;
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,34 +1,38 @@
 import ContactmeForm from "../components/ContactmeForm";
-import classes from './ContactPage.module.css';
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import classes from "./ContactPage.module.css";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ContactPage() {
-    return (
-        <section>
-            <ContactmeForm />
-            <div className={classes.iconContainer}>
+  return (
+    <section>
+      <ContactmeForm />
+      <div className={classes.iconContainer}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/adam780930"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className={classes.github}
+          ></FontAwesomeIcon>
+        </a>
 
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/adam780930">
-                    <FontAwesomeIcon
-                        icon={faGithub}
-                        className={classes.github}>
-                    </FontAwesomeIcon>
-                </a>
-
-                <a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/adam-tsai-008377133/">
-                    <FontAwesomeIcon
-                        icon={faLinkedin}
-                        className={classes.linkedin}>
-                    </FontAwesomeIcon>
-                </a>
-
-            
-
-            </div>
-        </section>
-    );
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/adam-yilin-tsai"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className={classes.linkedin}
+          ></FontAwesomeIcon>
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default ContactPage;
