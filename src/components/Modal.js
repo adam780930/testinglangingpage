0f8adb374a2a1ef342f1b9c146c6332ddb1b59ae
Adam Tsai <adam78030@gmail.com>
2024-09-02 17:56:56 -0700

    updated modal styling

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import classes from './Modal.module.css';
-import PdfComponent from './PdfComponent';
-import PdfResume from '../materials/resume.pdf';
+import React from "react";
+import classes from "./Modal.module.css";
+import PdfComponent from "./PdfComponent";
+import PdfResume from "../materials/resume.pdf";
 
 function Modal({ closeModal }) {
-
-    return (
-        <div className={classes.modalBackground} onClick={() => closeModal(false)}>
-            <div className={classes.modalContainer} onClick={e => e.stopPropagation()}>
-                <div className={classes.titleCloseBtn}>
-                    <button onClick={() => closeModal(false)}> x </button>
-                </div>
-                <div>
-                    <h1 className={classes.title}>My Resume</h1>
-                </div>
-                <div className={classes.body}>
-                <PdfComponent />
-                </div>
-                <div className={classes.footer}>
-                <a target="_blank" rel="noopener noreferrer" href={PdfResume}>Download</a>
-                </div>
+  return (
+    <div class="container">
+      <div class="modal-content">
+        <div  onClick={() => closeModal(false)}>
+          <div onClick={(e) => e.stopPropagation()}>
+            <div>
+              <button onClick={() => closeModal(false)}> x </button>
             </div>
+            <div>
+              <h1>My Resume</h1>
+            </div>
+            <div class="center">
+              <PdfComponent />
+            </div>
+            <div>
+              <a target="_blank" rel="noopener noreferrer" href={PdfResume}>
+                Download
+              </a>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Modal;
