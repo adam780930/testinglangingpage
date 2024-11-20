c354dc7ae52f957b5c147978d286499f4ecf9db6
Adam <100063577+adam780930@users.noreply.github.com>
2024-11-19 21:24:56 -0800

    updated styling on about me page (#5)
    * updated styling on about me page
    
    * minor styling update

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -3,26 +3,29 @@ import pfp2 from "../materials/pfp2.jpg";
 
 function AboutPage() {
   return (
-    <div className={classes.content}>
-      <div class="container-fluid">
-        <div class="row">
+    <div>
+      <div class="container-lg">
+        <div class="row py-5">
           <div class="col-6">
-            <img src={pfp2} className={classes.portrait} alt="" ></img>
+            <img src={pfp2} className={classes.portrait} alt=""></img>
           </div>
-          
-          <div class="col-4">
-            <header className={classes.text}>
-              <span>Hi, I am Adam</span> <br></br>I am a passionate, highly
-              motivated and self-driven Full-Stack software engineer proficient
-              in using JavaScript, TypeScript, React, Angular, HTML, CSS, Rest
-              API, Node.JS, Express, PostgreSQL, and frameworks like Bootstrap.
-              <br></br>
-              <br></br>I am dedicated to continuous learning and improvement in
-              the field of web development, and I am always seeking new
-              challenges and opportunities to grow. Connect with me on LinkedIn,
-              check out my GitHub profile, or view my portfolio to see more of
-              my work.
-            </header>
+
+          <div class="col-6 my-auto">
+            <div>
+              <span class="display-3 bg-dark text-light">Hi, I am Adam</span>
+              <p class="h4 bg-light text-dark px-2">
+                <br></br>I am a passionate, highly motivated and self-driven
+                Full-Stack software engineer proficient in using JavaScript,
+                TypeScript, React, Angular, HTML, CSS, Rest API, Node.JS,
+                Express, PostgreSQL, and frameworks like Bootstrap.
+                <br></br>
+                <br></br>I am dedicated to continuous learning and improvement
+                in the field of web development. I am always seeking new
+                challenges and opportunities to grow. Connect with me on
+                LinkedIn, check out my GitHub profile, or view my portfolio to
+                see more of my work.
+              </p>
+            </div>
           </div>
         </div>
       </div>
