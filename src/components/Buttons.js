6be1a52a1f738390e3533145ad140096212129ab
Adam Tsai <adam780930@gmail.com>
2024-12-26 14:47:48 -0800

    removed unused css files

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,16 +1,10 @@
-import classes from './Buttons.module.css';
-
-function buttons({linkOpen, downloadResume}) {
-    return (
-        <div className={classes.btn}>
-          <h2 className={classes.projects} onClick={linkOpen}>
-            Projects
-          </h2>
-          <h2 className={classes.resume} onClick={downloadResume}>
-            Experiences
-          </h2>
-        </div>
-    );
+function buttons({ linkOpen, downloadResume }) {
+  return (
+    <div>
+      <h2 onClick={linkOpen}>Projects</h2>
+      <h2 onClick={downloadResume}>Experiences</h2>
+    </div>
+  );
 }
 
 export default buttons;
--- src/components/ContactmeForm.js
+++ src/components/ContactmeForm.js
@@ -1,46 +1,57 @@
-import classes from './ContactmeForm.module.css';
-import emailjs from 'emailjs-com';
+import classes from "./ContactmeForm.module.css";
+import emailjs from "emailjs-com";
 
 function ContactmeForm() {
-    function Emailsubmit (e) {
+  function Emailsubmit(e) {
     e.preventDefault();
 
-    emailjs.sendForm('service_xtgzfki', 'template_9ll93ma', e.target, 'user_kh1diuAotFnI96MCgonLa')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset();
-        alert('Thank you for your message!');
-    }
+    emailjs
+      .sendForm(
+        "service_xtgzfki",
+        "template_9ll93ma",
+        e.target,
+        "user_kh1diuAotFnI96MCgonLa"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+    alert("Thank you for your message!");
+  }
 
-    return (
-        <section>
-            <header className={classes.title}><span>Contact me</span></header>
-        <form className={classes.form} onSubmit={Emailsubmit}>
-            <div className={classes.control}>
-                <label htmlFor="name">Your Name</label>
-                <input type="text" required id="name" name="name" />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="email">Your Email</label>
-                <input type="email" required id="email" name="email" />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="subject">Subject</label>
-                <input type="text" required id="subject" name="subject" />
-            </div>
-            <div className={classes.control}>
-                <label htmlFor="message">Message</label>
-                <textarea id="message" required row="5" name="message"></textarea>
-            </div>
-            <div className={classes.actions}>
-                <button>Send Message</button>
-            </div>
-        </form>
-        </section>
-    );
+  return (
+    <section>
+      <header className={classes.title}>
+        <span>Contact me</span>
+      </header>
+      <form className={classes.form} onSubmit={Emailsubmit}>
+        <div className={classes.control}>
+          <label htmlFor="name">Your Name</label>
+          <input type="text" required id="name" name="name" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" required id="email" name="email" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="subject">Subject</label>
+          <input type="text" required id="subject" name="subject" />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="message">Message</label>
+          <textarea id="message" required row="5" name="message"></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default ContactmeForm;
