92953afcf3a65829beb5209fa2eb496eef17a607
Adam Tsai <adam780930@gmail.com>
2024-12-26 14:29:34 -0800

    added md file for about this website, removed commented out codes that are no longer being used for this project

---
+++ src/components/markdowndocs/Aboutthiswebsite.md
@@ -0,0 +1,5 @@
+This website is created with React, utilizing useState/ useEffect hooks, styled with BootStrap framework and built using the following libraries:
+- react-router-dom
+- react-markdown
+- react-pdf
+- emailjs
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,34 +1,32 @@
 import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 import classes from "./HomePage.module.css";
 import Portrait from "../components/Portrait";
 import ProjectExamples from "../components/ProjectExamples";
 import ResumeModal from "../components/ResumeModal";
-// import PdfResume from "../materials/resume.pdf";
-// import Buttons from "../components/Buttons";
-// import Modal from "../components/Modal";
-
+import aboutthissite from "../components/markdowndocs/Aboutthiswebsite.md";
 
 function HomePage() {
+  const [aboutThisWebsiteContent, setaboutThisWebsiteContent] = useState("");
+
+  fetch(aboutthissite)
+    .then((response) => response.text())
+    .then((text) => setaboutThisWebsiteContent(text))
+    .catch((error) => console.error("Error fetching markdown:", error));
+
   function linkOpen() {
     window.open("https://project.adamtsaidev.com");
   }
-
-  // function downloadResume() {
-  //   window.open(PdfResume);
-  // }
-
-  // function resumeOpen() {
-  //   setOpenModal(true);
-  // }
-
-  // const [openModal, setOpenModal] = useState(false);
 
   return (
     <div>
       <div className={classes.title}>
         Welcome to my landing page
         <Portrait />
-        <ResumeModal linkOpen={linkOpen}/>
+        <ResumeModal linkOpen={linkOpen} />
+        <p class="h6 col-8 bg-light text-left text-dark border border-dark rounded mx-auto p-2 mt-2">
+          <ReactMarkdown>{aboutThisWebsiteContent}</ReactMarkdown>
+        </p>
         <ProjectExamples />
       </div>
     </div>
